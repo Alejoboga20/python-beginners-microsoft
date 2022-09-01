9e6a1777fb70866099e8a20b9d9d343901a83780
Alejo Boga <alejoboga20@gmail.com>
2022-09-01 09:01:20 -0400

    Math operations

---
+++ module7/app.py
@@ -0,0 +1,25 @@
+a = 10
+b = 2
+
+
+# Basic Operations
+sum = a + b
+difference = a - b
+multiplication = a * b
+division = a / b
+
+print(f'Sum: {sum}, Diff: {difference}, Mult: {multiplication}, Div: {division}')
+
+# Working with division
+seconds = 1042
+display_minutes = 1042 // 60 # Floor Division
+display_seconds = 1042 % 60 # Module
+print(display_minutes)
+print(display_seconds)
+
+# convert strings into numbers
+demo_int = int('215')
+print(demo_int)
+
+demo_float = float('215.3')
+print(demo_float)
