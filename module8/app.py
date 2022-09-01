d162a08ed8d89f3d936dccfa664be9370833c94b
Alejo Boga <alejoboga20@gmail.com>
2022-09-01 09:54:52 -0400

    Manipulate arrays

--- module8/app.py
+++ module8/app.py
@@ -20,3 +20,37 @@ print("The penultimate planet is", planets[-2])
 # Find an item by value
 jupiter_index = planets.index("Jupiter") # Returns the index of jupiter element
 print("Jupiter is the", jupiter_index + 1, "planet from the sun")
+
+
+# Number list
+gravity_on_earth = 1.0
+gravity_on_the_moon = 0.166
+gravity_on_planets = [0.378, 0.907, 1, 0.377, 2.36, 0.916, 0.889, 1.12]
+
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("On Mercury, a double-decker bus weighs", bus_weight * gravity_on_planets[0], "kg")
+
+bus_weight = 12650 # in kilograms, on Earth
+
+print("On Earth, a double-decker bus weighs", bus_weight, "kg")
+print("The lightest a bus would be in the solar system is", bus_weight * min(gravity_on_planets), "kg")
+print("The heaviest a bus would be in the solar system is", bus_weight * max(gravity_on_planets), "kg")
+
+# Manipulate data
+planets_before_earth = planets[0:2]
+planets_after_earth = planets[3:] # The same as planets[3: last index]
+print('Planets before earth: ', planets_before_earth)
+print('Planets after earth: ', planets_after_earth)
+
+amalthea_group = ["Metis", "Adrastea", "Amalthea", "Thebe"]
+galilean_moons = ["Io", "Europa", "Ganymede", "Callisto"]
+
+regular_satellite_moons = amalthea_group + galilean_moons
+print(regular_satellite_moons)
+
+# Sort
+
+planets.sort() # Modify the list
+print(planets)
