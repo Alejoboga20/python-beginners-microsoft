a6b68b70115492693048a9dffa9bc9604cb15cda
Alejo Boga <alejoboga20@gmail.com>
2022-09-01 13:53:00 -0400

    Working with loops

--- module9/app.py
+++ module9/app.py
@@ -1,3 +1,6 @@
+# While loop
+from time import sleep
+
 new_planet = ''
 planets = []
 
@@ -7,3 +10,12 @@ while new_planet.lower() != 'done':
     new_planet = input('Enter a new planet, or done if done: ')
 
 print(planets)
+
+# For Loop
+planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"]
+countdown = [4, 3, 2, 1, 0]
+
+for number in countdown:
+    print(number)
+    sleep(1)  # Wait 1 second
+print("Blast off!! 🚀")
