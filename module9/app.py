3d50891905c02b357f7a48de442b2bf31ee133b2
Alejo Boga <alejoboga20@gmail.com>
2022-09-01 10:09:56 -0400

    While loop

---
+++ module9/app.py
@@ -0,0 +1,9 @@
+new_planet = ''
+planets = []
+
+while new_planet.lower() != 'done':
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input('Enter a new planet, or done if done: ')
+
+print(planets)
